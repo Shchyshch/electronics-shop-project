3a818dcd9f8c9b1b96d0f57bc7a354617585f0f3
Ildar Gabdullin <ildar8803@gmail.com>
2023-06-16 16:10:50 +0300

    Add second homework

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,34 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            raise Exception('Длина наименования товара превышает 10 символов')
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all = []
+        with open('../src/items.csv', newline='') as csvfile:
+            data = csv.DictReader(csvfile)
+            for row in data:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(str_):
+        return int(float(str_))
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -16,3 +16,19 @@ def test_apply_discount(test_item):
     Item.pay_rate = 0.1
     test_item.apply_discount()
     assert test_item.calculate_total_price() == 70_000
+
+
+def test_name(test_item):
+    test_item.name = 'Пылесос'
+    assert test_item.name == 'Пылесос'
+    with pytest.raises(Exception):
+        test_item.name = 'Вакуумный пылесос'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('17.777') == 17
